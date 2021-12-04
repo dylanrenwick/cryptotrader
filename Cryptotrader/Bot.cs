3cfb3704bf0bb88954ebe2005b5a4f9aafb7f60a
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-04 00:17:00 -0500

    Added BotState to Bot

--- Cryptotrader/Bot.cs
+++ Cryptotrader/Bot.cs
@@ -2,9 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Cryptotrader.State;
+
 namespace Cryptotrader
 {
     public class Bot
     {
+        private DateTime lastStateChange;
+
+        private BotState initialState;
+        private BotState activeState;
+
+        public void Startup()
+        {
+            if (initialState == null) throw new InvalidOperationException("Initial State not set!");
+            SetState(initialState);
+        }
+
+        public void Update()
+        {
+            activeState.Update();
+        }
+
+        private void SetState(BotState newState)
+        {
+            activeState?.ExitState();
+            activeState = newState;
+            activeState.EnterState();
+            lastStateChange = DateTime.Now;
+        }
     }
 }
