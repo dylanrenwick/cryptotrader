b8d02c0f04f1f4584ba55f5de12a9cc7db0d70ca
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-04 00:02:57 -0500

    Added Product property to CoinbaseDataProvider

---
+++ Cryptotrader/CoinbaseDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptotrader
+{
+    public class CoinbaseDataProvider
+    {
+        public CryptoProduct Product { get; private set; }
+
+        public CoinbaseDataProvider(CryptoProduct product)
+        {
+            Product = product;
+        }
+        public CoinbaseDataProvider(string product)
+        {
+            Product = new CryptoProduct(product);
+        }
+    }
+}
