c474a6a5db6009451d5a2a0d8ef716c4cb543b24
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-04 00:10:00 -0500

    Added BotState

---
+++ Cryptotrader/BotState/BotState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptotrader.BotState
+{
+    public abstract class BotState
+    {
+        public Bot Bot { get; private set; }
+
+        public BotState(Bot bot)
+        {
+            Bot = bot;
+        }
+
+        public abstract void EnterState();
+        public abstract void ExitState();
+
+        public abstract void Update();
+    }
+}
