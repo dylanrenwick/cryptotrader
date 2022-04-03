507635dbf4f48431ab297e87efe95020a957478d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-04-03 03:44:05 -0400

    Began work on startup

--- Cryptotrader/Program.cs
+++ Cryptotrader/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+using Cryptotrader.Coinbase;
+using Cryptotrader.Config;
+using Cryptotrader.Logging;
 
 namespace Cryptotrader
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string CONFIG_PATH = @"config.json";
+
+        private static readonly ConfigLoader cfgLoader = new();
+        private static Logger logger;
+
+        private static CoinbaseDataProvider coinbaseDataProvider;
+
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await LoadConfig();
+            logger = new Logger(cfgLoader.GetLogDestination());
+            logger.Debug("Logger initialized");
+
+            logger.Debug("Loading API...");
+            await CreateDataProvider();
+            logger.Debug("API Loaded");
+        }
+
+        private static async Task LoadConfig()
+        {
+            if (!await cfgLoader.LoadConfig(CONFIG_PATH)) throw new Exception("Could not load config");
+        }
+
+        private static async Task CreateDataProvider()
+        {
+            var apiConfig = cfgLoader.GetApiConfig();
+            var apiLogger = logger.Label("API");
+
+            coinbaseDataProvider = new(
+                apiLogger,
+                apiConfig.ApiKey,
+                apiConfig.ApiSecret,
+                apiConfig.ApiPassphrase,
+                apiConfig.Cash,
+                apiConfig.Coin
+            );
+
+            await coinbaseDataProvider.FetchAccounts();
         }
     }
 }
