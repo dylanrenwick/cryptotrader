63549537e1010a1d78d7ad984b561acb9ed90170
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-04-17 00:17:25 -0400

    Made HistoricalValue<T> public

--- Cryptotrader/HistoricalValue.cs
+++ Cryptotrader/HistoricalValue.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Cryptotrader
 {
-    internal class HistoricalValue<T>
+    public class HistoricalValue<T>
     {
         public T Current { get; private set; }
 
