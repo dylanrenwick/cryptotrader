b594d37cdd1ebcb121c603a2461e09bafad41126
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-04-17 04:31:13 -0400

    Added several upgrades to RequestResult

--- Cryptotrader/Api/RequestResult.cs
+++ Cryptotrader/Api/RequestResult.cs
@@ -8,6 +8,26 @@
 
         public bool IsSuccess { get; private set; }
 
+        public RequestResult<Y> CastTo<Y>(Func<T, Y> convertFunc)
+        {
+            if (IsSuccess)
+            {
+                return RequestResult<Y>.Success(convertFunc(Result));
+            }
+            else
+            {
+                return RequestResult<Y>.Failure(ErrorMessage);
+            }
+        }
+
+        public T Unwrap() => Unwrap(new ResultException(ErrorMessage));
+
+        public T Unwrap(Exception ex)
+        {
+            if (IsSuccess) return Result;
+            throw ex;
+        }
+
         public static RequestResult<T> Success(T t)
         {
             return new() {
@@ -24,5 +44,12 @@
                 ErrorMessage = message
             };
         }
+    }
+
+    public class ResultException : Exception
+    {
+        public ResultException(): base() { }
+        public ResultException(string message) : base(message) { }
+        public ResultException(string message, Exception inner) : base(message, inner) { }
     }
 }
